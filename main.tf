533f97c31408dc8bf9aede4d815b4d03c977923e
Matthew Peters <mpeters413@gmail.com>
2020-06-05 12:47:36 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -26,6 +26,4 @@ resource "azurerm_mysql_server" "example" {
   geo_redundant_backup_enabled      = true
   infrastructure_encryption_enabled = true
   public_network_access_enabled     = false
-  ssl_enforcement_enabled           = true
-  ssl_minimal_tls_version_enforced  = "TLS1_2"
 }
