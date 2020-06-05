d37fd295186bf3c5731fe1e44fd709911586f95b
Matthew Peters <mpeters413@gmail.com>
2020-06-05 12:35:33 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,8 +11,8 @@ resource "azurerm_resource_group" "example" {
 
 resource "azurerm_mysql_server" "example" {
   name                = "example-mysqlserver"
-  location            = azurerm_resource_group.matt.location
-  resource_group_name = azurerm_resource_group.matt.name
+  location            = azurerm_resource_group.example.east
+  resource_group_name = azurerm_resource_group.example.matt
 
   administrator_login          = "mysqladminun"
   administrator_login_password = "H@Sh1CoR3!"
