82bd771b067d928f4f74ebcb340726c0467ba881
Matthew Peters <mpeters413@gmail.com>
2020-06-05 12:37:55 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,18 +4,15 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "example" {
-  name     = "matt"
-  location = "east"
+resource "azurerm_resource_group" "test" {
+  name     = "api-rg-pro"
+  location = "West Europe"
 }
 
-resource "azurerm_mysql_server" "example" {
-  name                = "example-mysqlserver"
-  location            = azurerm_resource_group.example.east
-  resource_group_name = azurerm_resource_group.example.matt
-
-  administrator_login          = "mysqladminun"
-  administrator_login_password = "H@Sh1CoR3!"
+resource "azurerm_mysql_server" "test" {
+  name                = "mysql-server-1"
+  location            = "${azurerm_resource_group.test.location}"
+  resource_group_name = "${azurerm_resource_group.test.name}"
 
   sku_name   = "B_Gen5_2"
   storage_mb = 5120
