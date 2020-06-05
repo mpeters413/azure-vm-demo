47215bc9a03102d7b9fed1b3a4e6195e6070919e
Matthew Peters <mpeters413@gmail.com>
2020-06-05 12:05:10 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,8 +8,8 @@ provider "azurerm" {
 resource "azurerm_linux_virtual_machine" "myterraformvm" {
     name                  = "myVM"
     location              = "eastus"
-    resource_group_name   = azurerm_resource_group.myterraformgroup.name
-    network_interface_ids = [azurerm_network_interface.myterraformnic.id]
+    resource_group_name   = "mattsresource"
+    network_interface_ids = "mattsnetwork"
     size                  = "Standard_DS1_v2"
 
     os_disk {
