4555b8caa9b8a12f1c07cb6f15f15c86c2dfacec
Matthew Peters <mpeters413@gmail.com>
2021-08-03 16:29:04 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,38 +1,48 @@
-
-variable "location" {
-  description = "Azure location in which to create resources"
-  default = "East US"
+provider "azurerm" {
+  features {}
 }
 
-variable "windows_dns_prefix" {
-  description = "DNS prefix to add to to public IP address for Windows VM"
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "East US"
 }
 
-variable "admin_password" {
-  description = "admin password for Windows VM"
-  default = "pTFE1234!"
-}
-
-module "windowsserver" {
+module "linuxservers" {
   source              = "Azure/compute/azurerm"
-  version             = "1.1.5"
-  location            = "${var.location}"
-  resource_group_name = "${var.windows_dns_prefix}-rc"
-  vm_hostname         = "pwc-ptfe"
-  admin_password      = "${var.admin_password}"
+  resource_group_name = azurerm_resource_group.example.name
+  vm_os_simple        = "UbuntuServer"
+  public_ip_dns       = ["linsimplevmips"] // change to a unique name per datacenter region
+  vnet_subnet_id      = module.network.vnet_subnets[0]
+
+  depends_on = [azurerm_resource_group.example]
+}
+
+module "windowsservers" {
+  source              = "Azure/compute/azurerm"
+  resource_group_name = azurerm_resource_group.example.name
+  is_windows_image    = true
+  vm_hostname         = "mywinvm" // line can be removed if only one VM module per resource group
+  admin_password      = "ComplxP@ssw0rd!"
   vm_os_simple        = "WindowsServer"
-  public_ip_dns       = ["${var.windows_dns_prefix}"]
-  vnet_subnet_id      = "${module.network.vnet_subnets[0]}"
+  public_ip_dns       = ["winsimplevmips"] // change to a unique name per datacenter region
+  vnet_subnet_id      = module.network.vnet_subnets[0]
+
+  depends_on = [azurerm_resource_group.example]
 }
 
 module "network" {
   source              = "Azure/network/azurerm"
-  version             = "1.1.1"
-  location            = "${var.location}"
-  resource_group_name = "${var.windows_dns_prefix}-rc"
-  allow_ssh_traffic   = true
+  resource_group_name = azurerm_resource_group.example.name
+  subnet_prefixes     = ["10.0.1.0/24"]
+  subnet_names        = ["subnet1"]
+
+  depends_on = [azurerm_resource_group.example]
 }
 
-output "windows_vm_public_name"{
-  value = "${module.windowsserver.public_ip_dns_name}"
+output "linux_vm_public_name" {
+  value = module.linuxservers.public_ip_dns_name
+}
+
+output "windows_vm_public_name" {
+  value = module.windowsservers.public_ip_dns_name
 }
