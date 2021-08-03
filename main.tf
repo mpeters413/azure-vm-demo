826b02238b90a03b587bfc9f8bf6bd0c7d92f2af
Matthew Peters <mpeters413@gmail.com>
2021-08-03 16:19:46 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,157 +1,41 @@
-
-provider "azurerm" {
-  # whilst the `version` attribute is optional, we recommend pinning to a given version of the Provider
-  version = "=2.0.0"
-  features {}
+terraform {
+  required_version = ">= 0.11.1"
 }
 
-# Create a resource group if it doesn't exist
-resource "azurerm_resource_group" "myterraformgroup" {
-    name     = "myResourceGroup"
-    location = "eastus"
-
-    tags = {
-        environment = "Terraform Demo"
-    }
+variable "location" {
+  description = "Azure location in which to create resources"
+  default = "East US"
 }
 
-# Create virtual network
-resource "azurerm_virtual_network" "myterraformnetwork" {
-    name                = "myVnet"
-    address_space       = ["10.0.0.0/16"]
-    location            = "eastus"
-    resource_group_name = azurerm_resource_group.myterraformgroup.name
-
-    tags = {
-        environment = "Terraform Demo"
-    }
+variable "windows_dns_prefix" {
+  description = "DNS prefix to add to to public IP address for Windows VM"
 }
 
-# Create subnet
-resource "azurerm_subnet" "myterraformsubnet" {
-    name                 = "mySubnet"
-    resource_group_name  = azurerm_resource_group.myterraformgroup.name
-    virtual_network_name = azurerm_virtual_network.myterraformnetwork.name
-    address_prefix       = ["10.0.1.0/24"]
+variable "admin_password" {
+  description = "admin password for Windows VM"
+  default = "pTFE1234!"
 }
 
-# Create public IPs
-resource "azurerm_public_ip" "myterraformpublicip" {
-    name                         = "myPublicIP"
-    location                     = "eastus"
-    resource_group_name          = azurerm_resource_group.myterraformgroup.name
-    allocation_method            = "Dynamic"
-
-    tags = {
-        environment = "Terraform Demo"
-    }
+module "windowsserver" {
+  source              = "Azure/compute/azurerm"
+  version             = "1.1.5"
+  location            = "${var.location}"
+  resource_group_name = "${var.windows_dns_prefix}-rc"
+  vm_hostname         = "pwc-ptfe"
+  admin_password      = "${var.admin_password}"
+  vm_os_simple        = "WindowsServer"
+  public_ip_dns       = ["${var.windows_dns_prefix}"]
+  vnet_subnet_id      = "${module.network.vnet_subnets[0]}"
 }
 
-# Create Network Security Group and rule
-resource "azurerm_network_security_group" "myterraformnsg" {
-    name                = "myNetworkSecurityGroup"
-    location            = "eastus"
-    resource_group_name = azurerm_resource_group.myterraformgroup.name
-    
-    security_rule {
-        name                       = "SSH"
-        priority                   = 1001
-        direction                  = "Inbound"
-        access                     = "Allow"
-        protocol                   = "Tcp"
-        source_port_range          = "*"
-        destination_port_range     = "22"
-        source_address_prefix      = "*"
-        destination_address_prefix = "*"
-    }
-
-    tags = {
-        environment = "Terraform Demo"
-    }
+module "network" {
+  source              = "Azure/network/azurerm"
+  version             = "1.1.1"
+  location            = "${var.location}"
+  resource_group_name = "${var.windows_dns_prefix}-rc"
+  allow_ssh_traffic   = true
 }
 
-# Create network interface
-resource "azurerm_network_interface" "myterraformnic" {
-    name                      = "myNIC"
-    location                  = "eastus"
-    resource_group_name       = azurerm_resource_group.myterraformgroup.name
-
-    ip_configuration {
-        name                          = "myNicConfiguration"
-        subnet_id                     = azurerm_subnet.myterraformsubnet.id
-        private_ip_address_allocation = "Dynamic"
-        public_ip_address_id          = azurerm_public_ip.myterraformpublicip.id
-    }
-
-    tags = {
-        environment = "Terraform Demo"
-    }
-}
-
-# Connect the security group to the network interface
-resource "azurerm_network_interface_security_group_association" "example" {
-    network_interface_id      = azurerm_network_interface.myterraformnic.id
-    network_security_group_id = azurerm_network_security_group.myterraformnsg.id
-}
-
-# Generate random text for a unique storage account name
-resource "random_id" "randomId" {
-    keepers = {
-        # Generate a new ID only when a new resource group is defined
-        resource_group = azurerm_resource_group.myterraformgroup.name
-    }
-    
-    byte_length = 8
-}
-
-# Create storage account for boot diagnostics
-resource "azurerm_storage_account" "mystorageaccount" {
-    name                        = "diag${random_id.randomId.hex}"
-    resource_group_name         = azurerm_resource_group.myterraformgroup.name
-    location                    = "eastus"
-    account_tier                = "Standard"
-    account_replication_type    = "LRS"
-
-    tags = {
-        environment = "Terraform Demo"
-    }
-}
-
-# Create (and display) an SSH key
-resource "tls_private_key" "example_ssh" {
-  algorithm = "RSA"
-  rsa_bits = 4096
-}
-output "tls_private_key" { value = "${tls_private_key.example_ssh.private_key_pem}" }
-
-# Create virtual machine
-resource "azurerm_linux_virtual_machine" "myterraformvm" {
-    name                  = "myVM"
-    location              = "eastus"
-    resource_group_name   = azurerm_resource_group.myterraformgroup.name
-    network_interface_ids = [azurerm_network_interface.myterraformnic.id]
-    size                  = "Standard_DS1_v2"
-
-    os_disk {
-        name              = "myOsDisk"
-        caching           = "ReadWrite"
-        storage_account_type = "Premium_LRS"
-    }
-
-    source_image_reference {
-        publisher = "Canonical"
-        offer     = "UbuntuServer"
-        sku       = "16.04.0-LTS"
-        version   = "latest"
-    }
-
-    computer_name  = "myvm"
-    admin_username = "azureuser"
-    disable_password_authentication = true
-        
- 
-
-    tags = {
-        environment = "Terraform Demo"
-    }
+output "windows_vm_public_name"{
+  value = "${module.windowsserver.public_ip_dns_name}"
 }
